4e242f473eede83df04e81a1b5bad3163e7a2228
Rafael Kaustchr <rafael.g@upland.me>
2023-12-01 10:23:55 -0800

    [2023] add utils

---
+++ 2023/utils/utils.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const readline = require('readline');
+
+/* UTILS // */ 
+async function getFileStream(path) {
+    const fileStream = fs.createReadStream(path);
+    return fileStream;
+}
+
+async function getFileStreamLines(path) {
+    const fileStream = await getFileStream(path);
+
+    // Note: we use the crlfDelay option to recognize all instances of CR LF
+    // ('\r\n') in input.txt as a single line break.
+    const rl = readline.createInterface({
+        input: fileStream,
+        crlfDelay: Infinity
+    });
+
+    return rl;
+}
+/* // UTILS */
+
+module.exports = {
+    getFileStream,
+    getFileStreamLines
+}
