4abc60c4315a613e7c6d178abd8958fd02586fc3
Rafael Kaustchr <rafael.g@upland.me>
2023-12-09 08:04:29 -0800

    [2023] day 09

---
+++ 2023/day-09/solution_part1.js
@@ -0,0 +1,43 @@
+const util = require('../utils/utils');
+
+async function main() {
+    const linesStream = await util.getFileStreamLines('./input.txt');
+
+    let total = 0;
+    for await (const line of linesStream) {
+        const nums = line.split(' ').map(Number);
+        total += proccess(nums)
+    }
+    
+    console.log("Result: %d", total);
+
+    // LOG STATS
+    const used = process.memoryUsage().heapUsed / 1024 / 1024;
+    console.log(`\n\nThe script uses approximately ${Math.round(used * 100) / 100} MB`);
+}
+
+main();
+
+function proccess(arr) {
+    const lastElems = [  ];
+    let isAllZero;
+    do {
+        lastElems.push( arr[ arr.length - 1 ] );
+        isAllZero = true;
+        const next = [];
+        for (let i=0; i < arr.length - 1; i++) {
+            const diff = arr[i+1] - arr[i];
+            next.push(diff);
+            if (diff != 0)
+                isAllZero = false;
+        }
+        arr = next;
+    } while (!isAllZero);
+
+    // extrapolates
+    let nextVal = 0;
+    while ( lastElems.length )
+        nextVal = lastElems.pop() + nextVal;
+
+    return nextVal;
+}
